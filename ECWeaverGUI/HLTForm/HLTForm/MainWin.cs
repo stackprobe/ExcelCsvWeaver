456f68bee5bbde1e90d1ae4561c0e0ab0a34ee30
stackprobe <stackprobe@gmail.com>
2026-08-20 20:46:55 +0900

    add:25 mod:0 del:0 mv:0 +7841 -0 2026/08/20 20:46:55 (+0:00:16)

---
+++ ECWeaverGUI/HLTForm/HLTForm/MainWin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HLTStudio.Commons;
+
+namespace HLTStudio
+{
+	public partial class MainWin : Form
+	{
+		public MainWin()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWin_Load(object sender, EventArgs e)
+		{
+			this.MinimumSize = this.Size;
+		}
+	}
+}
---
+++ ECWeaverGUI/HLTForm/HLTForm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HLTStudio.Commons;
+
+namespace HLTStudio
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.CUIMain(ar =>
+			{
+				GUIProcMain.GUIMain(() => new MainWin());
+			});
+		}
+	}
+}
